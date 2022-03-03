5c7e28ce3bb4d33650fd0063a3136bb70a61aa4e
RiccardoP01 <riccardo.petri01@universitadipavia.it>
2022-03-03 17:24:01 +0100

    Add project files.

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace FirstProgramsSolution
+{
+    class Program
+    {
+
+        
+
+        //static void Main(string[] args)
+        static void Main()
+        {
+            //Console.Write("Enter a number: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter a number: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter an operator: ");
+            //string operation = Console.ReadLine();
+
+
+            //Console.WriteLine("The result is " + SwitchMethod(operation, num1, num2));
+
+
+            int[,] matrix = {
+                {1,2,3},
+                {4,5,6}
+
+            };
+            Console.WriteLine(matrix[0, 2]);
+
+
+
+
+
+            try
+            {
+                int z = 0;
+                int prova = 5 / z;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("a prescindere");
+            }
+
+            //IEnumerable<T> //most generic, only if i want to output data
+            //ICollection<T> //it extends IEnumerable (i can manipulate more)
+            IList<int> list = new List<int> { 1, 2, 3, 4, 5 }; //it extends ICollection (allow to index elements)
+            foreach(var t in list)
+            {
+                Console.WriteLine(t);
+            }
+
+            //List<int> evenNumbers = new List<int>();
+            var evenNumbers = new List<int>();
+
+            foreach(var t in list)      //as suggestion, visual studio convert it as a LINQ
+            {
+                if(t % 2 == 0)
+                    evenNumbers.Add(t);
+            }
+
+            //______________________________________________________________________________________________________________________________________
+            //ENUMS NON VA
+
+            //enum move { None, Up, Down, Right, Left}
+
+            //foreach(String name in Enum.GetNames(typeof(Move)))
+            //{
+            //    Console.Write("{0}", name);
+            //}
+
+            //foreach(int value in Enum.GetValues(typeof(Move)))
+            //{
+            //    Console.Write("{0}", value);
+            //}
+
+
+            //______________________________________________________________________________________________________________________________________
+
+            //STRUCTS (non può ereditare, ha sempre il costruttore di default (classi solo se non hanno un altro costruttore se no lo perdono)
+                     //usare strutture se sono sicuro di fare qualcosa di immutato.
+
+
+            int number1 = int.MaxValue;  //int is a struct and MaxValue is a property!
+            string eleven = 11.ToString(); //string is class instead
+            double d;
+            double.TryParse(eleven, out d);
+            Console.WriteLine(d);
+
+            Point p1 = new Point(2, 3);
+            Console.WriteLine(string.Format("p1: {0}", p1));
+            Point p2 = new Point(); //default
+            Console.WriteLine(string.Format("p2: {0}", p2));
+
+            //______________________________________________________________________________________________________________________________________
+
+            //EVENTS
+            var rooster = new Rooster();
+            var person = new Person();
+            var person2 = new Person();
+            rooster.DoCaw();    //the event caw has no subscribers yet
+            rooster.Caw += person.SayImUp; //adding a subscriber ( a method that will be invoked)
+            rooster.Caw += person2.SayImUp2;
+            rooster.DoCaw();    //now it will invoke all of them
+
+             //______________________________________________________________________________________________________________________________________
+
+
+            Console.WriteLine(Radius2);
+
+            Console.ReadKey();
+        }
+
+
+        //static int SwitchMethod(string operation, int num1, int num2)
+        //{
+        //    switch(operation)
+        //    {
+        //        case "+":
+        //            return num1 + num2;
+        //            break;
+        //        case "-":
+        //            return num1 - num2;
+        //            break;
+        //        case "/":
+        //            return num1 / num2;
+        //            break;
+        //        case "*":
+        //            return num1 * num2;
+        //            break;
+        //        default:
+        //            return 0;
+        //    }
+        //}
+
+        
+
+        //______________________________________________________________________________________________________________________________________
+
+        //THREADS
+
+        //var thread = new Thread(run);
+        //thread.Start();
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Thread.Sleep(100);                          //in teoria uso due thread diversi
+        //    Console.WriteLine("Hello from thread 1");
+        //}
+        //thread.Join(); //per essere sicuri che il thread termini(?)
+
+
+        //______________________________________________________________________________________________________________________________________
+
+        //GETTER AND SETTER ARE USED WITH PROPERTIES!!!!       per avere l'incapsulamento e un accesso al posto dei MODIFICATORI DI ACCESSO public private protected
+
+        //private static double ciao;  //insert in the class program!!!
+        //public double Radius2 {
+        //    get {
+        //        return ciao;
+        //    }
+        //    set {
+        //        ciao = value;
+        //    }
+        //}
+
+        //BECOME
+
+        //public double Radius1 { get; private set; }
+        public static double Radius2 { get; private set; } = 3.5;       //static only because i call it from main that is static!!!
+                                                                        //public ClassType Radius2 { get; private set; } = 3.5;
+
+        //______________________________________________________________________________________________________________________________________
+
+
+        struct Point
+        {
+            public int X { get; }
+            public int Y { get; }
+
+            public Point (int x, int y) //constructor!!!
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override string ToString()
+            {
+                return $"({X},{Y})";        //$ to interpolate tostring!!
+            }
+        }
+
+        //______________________________________________________________________________________________________________________________________
+
+        class Rooster   //INNER CLASS
+        {
+            public event Action Caw;
+
+            public void DoCaw()
+            {
+                if(Caw != null) //if i have at least one subscriber
+                {
+                    Console.WriteLine("not null");
+                    Caw();
+                }
+                else
+                {
+                    Console.WriteLine("null");
+                }
+            }
+        }
+
+        class Person
+        {
+            public void SayImUp()
+            {
+                Console.WriteLine("buongiornooo");
+            }
+
+            public void SayImUp2()
+            {
+                Console.WriteLine("buongiornooooooooooooo");
+            }
+        }
+
+
+
+
+        private static void run()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("Hello from thread 2");
+            }
+        }
+    }
+}
+
+
+
---
+++ NullOperators/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+
+namespace NullOperators         //if i want to use this, i have to tap "Set as startupProject"!!!!! and bolding the NullPointer console app
+{
+    class Program       //Null-conditional operators
+    {
+        static void Main(string[] args)
+        {
+
+            //________________________________________________________________________________________________________________
+
+            //The Null-Conditional Operator
+            //Console.Write(myObject?.Items?[0].ToString());
+
+
+            //The Conditional Operator
+            //return isTrue ? "Valid" : "Lie";
+
+
+            //The Null Coalescing Operator
+            //return myObject ?? yourObject;    A ?? (B ?? C)
+
+
+            //Nullable Value Types Nullable<T>   ->         i can use x.hasValue (it is bool) also  x.getValueOrDefault(-1) if is null x = -1
+            //int? universalAnswer = 42;
+
+
+            //Nullable Reference Types
+            //string? value = "bob";
+
+
+            //Nullable array of nullable reference objects
+            //public static Delegate? Combine(Delegate?[]? delegates)
+
+            //Since each parameter is of the type Delegate? and you return an index of the Delegate?[]? array, then it makes sense that the return
+            //type is Delegate? otherwise the compiler would return an error as if you were returing and int from a method that returns a string.
+
+            //params Delegate?[] delegates(...)     It is an array of nullable Delegate
+            //params Delegate?[]? delegates         The entire array can be nullable
+            //________________________________________________________________________________________________________________
+
+
+            string str =null;
+
+            //if(str == null)
+            //{
+            //    str = "ciao";
+            //}
+
+            str ??= "ciao"; //if str is null, then set it as "ciao"
+
+            Console.WriteLine(str);
+
+
+            int? a = null;      //int? is a shortcut for Nullable<int> (if the left hand is null!!!
+            a ??= (-1 + 80);    // ??= for assignment  and  ?? to return (the right-hand element if the left-element is null)
+            int b = a ?? (-1+80);    //if a is null then set it as -1+80 (but i did the assignment at line 24, so will not change a again!!
+            Console.WriteLine(b);  // output: -1
+
+
+            //var a = model?.Value;
+            //var a = model == null ? null : model.Value;
+
+
+            //________________________________________________________________________________________________________________
+
+            //Creating a method inside another method is called local functions in C#.
+            //Using a method inside another method makes code easier to read. Local functions is just like lamda expressions.
+            //Local function is a private method.
+            int GetSumOfFirstTwoOrDefault(int[] numbers)
+            {
+                //If numbers evaluates to null, the result of numbers?.x or numbers?[x] is null and it doesnt evaluate .x , so that i can use ?? with numbers as null
+                if((numbers?.Length ?? 0) < 2)  //if i dont use ??  ->     numbers?.Length  < 2   evaluate as false when numb is null
+                {
+                    return 0;
+                }
+                return numbers[0] + numbers[1];
+            }
+
+            Console.WriteLine(GetSumOfFirstTwoOrDefault(null));  // output: 0
+            Console.WriteLine(GetSumOfFirstTwoOrDefault(new int[0]));  // output: 0
+            Console.WriteLine(GetSumOfFirstTwoOrDefault(new[] { 3, 4, 5 }));  // output: 7
+
+
+
+            //________________________________________________________________________________________________________________
+
+            string manyLines = @"This is line one
+                                This is line two
+                                Here is line three
+                                The penultimate line is line four
+                                This is the final, fifth line.";
+
+            using var reader = new StringReader(manyLines);
+            string item;
+            do
+            {
+                item = reader.ReadLine();
+                Console.WriteLine(item);
+            } while(item != null);
+        }
+    }
+}
